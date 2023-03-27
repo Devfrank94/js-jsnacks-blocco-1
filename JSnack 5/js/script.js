34e6e7c3aa6c4e983d7b906fc6f8f89563f18895
Devfrank94 <francescomurro94@gmail.com>
2023-03-27 23:01:07 +0200

    end snack 5

---
+++ JSnack 5/js/script.js
@@ -0,0 +1,24 @@
+/*JSnack 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array */
+
+const array = [];
+let numIns;
+const output = document.getElementById('output');
+
+let i = 0;
+
+for (let i = 0; i < 6; i++) {
+  let numIns = parseInt(prompt("Scrivi un numero"));
+  
+  if (!(numIns % 2)) {
+    console.log("Numero pari")
+  }else {
+    array.push(numIns)
+  }
+}
+console.log(array)
+
+const message = `I numeri dispari da te inseriti sono ${array}`;
+document.getElementById('output').innerHTML = message;
