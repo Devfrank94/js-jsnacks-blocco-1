723a2cf665c5145af90966bb4769cd69bd082c74
Devfrank94 <francescomurro94@gmail.com>
2023-03-28 13:27:40 +0200

    fix snack 6

--- JSnack 6/js/script.js
+++ JSnack 6/js/script.js
@@ -1,12 +1,10 @@
 /*JSnack 6
 Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.*/
 
-const numIns = parseInt(prompt("inserisci un numero"));
+const limit = parseInt(prompt('Quanti cubi?'))
 
-let cubo = numIns * numIns * numIns;
-
-for (let i = 0; i <= numIns; i++)
-
-const message = `Il cubo del numero inserito è ${cubo}`;
-
-document.getElementById('output').innerHTML = message;
+for (let i = 1; i <= limit; i++) {
+  const cubo = Math.pow(i, 3);
+  console.log(cubo)
+  
+}
